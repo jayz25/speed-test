f252f9bc11a7b320e78923f7cb85e0ac8d0d62cd
jayz25 <patiljayesh026@gmail.com>
2022-07-31 03:28:32 +0530

    Start function added - Start will used to start recording of speed - Input will be focused and timer will start according to start event

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,23 +1,18 @@
 import { useState } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const Timer = ({inputDisabled, setInputDisabled, key}) => {
-    // const [timeUp, setTimeup] = useState(false);
-    // const timesUp = () => {
-    //     //Change state to disabled
-    //     //Stop typing
-    //     // setTimeup(true)
-    //     setInputDisabled(true);
-    // }
+const Timer = ({inputDisabled, setInputDisabled, timerKey, isPlaying}) => {
+    
     return (
-        <div className="fixed bottom-20 left-20">
+        <div className="fixed bottom-20 w-full m-auto">
             <CountdownCircleTimer
-                key={key}
-                isPlaying
+                key={timerKey}
+                isPlaying = {isPlaying}
                 duration={60}
                 colors={['#004777', '#F7B801', '#A30000', '#A30000']}
                 colorsTime={[60, 45, 25, 10]}
-                onComplete={() => setInputDisabled(true)}
+                onComplete={() => setInputDisabled(true)} 
+                // Calling reset stats on complete here would be good?
             >
                 {({ remainingTime }) => (
                     <span className="text-4xl">{remainingTime}</span>   
--- components/TypeHere.tsx
+++ components/TypeHere.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Timer from './Timer';
 
 const TypeHere = () => {
-    const [key, setKey] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [timerKey, setTimerKey] = useState(0);
     const [typedData, setTypedData] = useState('');
     const [numberOfWordsTyped, setNumberOfWordsTyped] = useState(0);
     const [percentage, setPercentage] = useState(0);
-    const [inputDisabled, setInputDisabled] = useState(false);
+    const [inputDisabled, setInputDisabled] = useState(true);
+    const inputRef = useRef(null);
     const givenString = 'Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing'.split(' ')
 
-    function getData(e) {
+    const getData = (e) => {
         e.preventDefault();
         
         setTypedData(e.target.value);
@@ -29,18 +31,26 @@ const TypeHere = () => {
         setPercentage(matchedCount/numberOfWordsTyped * 100);
     }
 
-    function resetStats() {
+    const resetStats = () => {
         setPercentage(0);
         setNumberOfWordsTyped(0);
-        setInputDisabled(false);
+        setInputDisabled(true);
         setTypedData('');
-        setKey(prevKey => prevKey + 1);
+        setTimerKey(prevKey => prevKey + 1);
+        setIsPlaying(false);
+    }
+
+    const fireStartEvent = async () => {
+        setIsPlaying(true);
+        await setInputDisabled(false);
+        //Waiting here to let it disable then focus. Synchrounous does not work here.
+        inputRef.current.focus();
     }
 
     return (
         <div className='p-4'>
             <div className='w-[10px] h-[10px]'>
-            <Timer inputDisabled= {inputDisabled} setInputDisabled = {setInputDisabled} key = {key} />
+            <Timer inputDisabled={inputDisabled} setInputDisabled={setInputDisabled} timerKey={timerKey} isPlaying={isPlaying} />
 
             </div>
             <div className='w-1/5 m-auto'>
@@ -50,19 +60,21 @@ const TypeHere = () => {
             </div>
 
             <input 
-                className="w-full p-2 m-auto text-center border-b-2 focus:outline-none"
+                className="w-full p-2 m-auto text-center border-b-2"
                 placeholder="Type above text here"
                 onChange={getData}
                 disabled={inputDisabled}
+                ref={inputRef}
                 value={typedData}
             />
+            <button className='mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                onClick={fireStartEvent}
+            >Start
+            </button>
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                 onClick={resetStats}    
             >Reset
             </button>
-            <div className='w-1/2 m-auto' >
-
-            </div>
         </div>
     )
 }
