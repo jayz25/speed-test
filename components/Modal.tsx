f2f93cf910d9983a48a57252417aacad433d964e
jayz25 <patiljayesh026@gmail.com>
2022-08-10 01:41:13 +0530

    UI Styling Added - Added some colors, looking better imo C: - Added StatsPill Component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,18 +1,19 @@
 export const Modal = ({setModalVisibility}) => {
         return (
-            <div className="top-0 left-0 right-0 bottom-0 w-full h-full bg-neutral-700 fixed">
-                
-                <div className="modal-container absolute top-1/3  left-1/3 bg-zinc-200 rounded-md w-[600px] h-[200px]">
-                    <button className="block absolute right-0" onClick={() => setModalVisibility(false)}>
-                        <span className="font-bold text-xl float p-[10px]">x</span>
+            <>
+            {/* Add transition effect for modal opening and closing and get a good close icon */}
+            <div className="flex bg-[#EFEFEF] absolute top-0 right-0 left-0 bottom-0 justify-center bg-opacity-70 items-center">
+                <div className="bg-[#ffffff] rounded-md w-[600px] h-[200px] p-2 shadow-lg">
+                    <button className="block relative left-0" onClick={() => setModalVisibility(false)}>
+                        <span className="font-bold text-xl p-[10px]">x</span>
                     </button>
                     <div className="p-[10px]">
                         <p>
                         Hello This is a Modal Welcome To Modal
                         </p>
                     </div>
-                    
                 </div>
             </div>
+            </>
         )
 }
--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { Modal } from "./Modal";
 
-const Timer = ({inputDisabled, setInputDisabled, timerKey, isPlaying, setIsPlaying}) => {
+const Timer = ({inputDisabled, setInputDisabled, timerKey, isPlaying, setIsPlaying, setWpm}) => {
     const [isModalVisible, setModalVisibility] = useState(false);
     const onCompleteTimer = () => {
-        //Fire input Disabled event or reset stats 
+        //Fire input Disabled event or reset stats
         //Fire Modal dislay here
         setInputDisabled(true);
         setModalVisibility(true);
         setIsPlaying(false);
-        //Can Call reset stats here 
+        //Can Call reset stats here
     }
     return (
         <>
         {(isModalVisible ? <Modal setModalVisibility={setModalVisibility}></Modal> :
-        <div className="fixed bottom-20 w-full m-auto"> 
-            {/* This condition !isModaiVisible causing reset timer to bug out as it gets called again and again once we close modal 
+        <div className="flex justify-center p-4">
+            {/* This condition !isModaiVisible causing reset timer to bug out as it gets called again and again once we close modal
                 due to state change in react, it's gonna re-render again and again otherwise find out why */}
             <CountdownCircleTimer
                 key={timerKey}
@@ -24,12 +24,12 @@ const Timer = ({inputDisabled, setInputDisabled, timerKey, isPlaying, setIsPlayi
                 duration={5}
                 colors={['#004777', '#F7B801', '#A30000', '#A30000']}
                 colorsTime={[60, 45, 25, 10]}
-                onComplete={onCompleteTimer} 
+                onComplete={onCompleteTimer}
                 // Calling reset stats on complete here would be good?
             >
                 {
                     ({ remainingTime }) => (
-                        <span className="text-4xl">{remainingTime}</span>   
+                        <span className="text-4xl">{remainingTime}</span>
                     )
                 }
             </CountdownCircleTimer>
