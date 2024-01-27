afb6601e38848399aa0c23e4e673dd3aa742ea79
Jayesh Patil <60577923+jayz25@users.noreply.github.com>
2024-01-27 18:13:53 +0530

    Custom API removal for paragraph and minor bug fix (#14)
    - Using hardcoded paragraphs from now on.
        -- this will be replaced with random words API or custom words generator
    - Bug fixes
        -- First letter and first word styles were applying incorrectly when they were mismatched.
        -- This is because if condition considers initial indices "0" as a falsy value. (used != null instead)

--- utils/removeStyling.ts
+++ utils/removeStyling.ts
@@ -1,4 +1,5 @@
 // Find a better way to do this please
+<<<<<<< Updated upstream
 export const removeStyling = () => {
     const container = document.querySelector<HTMLElement>("#given-paragraph");
     container
@@ -14,6 +15,37 @@ export const removeStyling = () => {
                 "text-slate-700",
                 "text-white",
               );
+=======
+export const removeStyling = (backSpaceStyling = false, letterIndex = null, wordIndex = null) => {
+  const container = document.querySelector<HTMLElement>("#given-paragraph");
+  if (backSpaceStyling && letterIndex != null && wordIndex != null) {
+      const activeWordEl = container.querySelectorAll<HTMLElement>("#word-element")[wordIndex]
+      const activeLetterEl = activeWordEl.querySelectorAll<HTMLElement>("#letter-element #letter")[letterIndex]
+      activeLetterEl.classList.remove(
+        "text-red-700",
+      )
+    }
+    else {
+      container
+        .querySelectorAll<HTMLElement>("#word-element")
+        .forEach((element) => {  
+          element.classList.remove("line-through")
+          element.querySelectorAll("#letter-element #letter")
+            .forEach((letterElement) => {
+              letterElement
+                .classList.remove(
+                  "bg-yellow-300",
+                  "text-red-700",
+                  "text-slate-700",
+                  "text-white",
+                );
+            });
+  
+          element.querySelectorAll("#letter-element #cursor").forEach((cursorElement) => {
+            cursorElement.classList.add(
+              "hidden"
+            );
+>>>>>>> Stashed changes
           });
 
         element.querySelectorAll("#letter-element #cursor").forEach((cursorElement) => {
